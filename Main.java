f65ab958f66941d862feee139d2a23a31fe7d268
SC-314 <scmgaming144@gmail.com>
2025-02-16 14:00:17 +0000

    Cleared all files

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String name = "username_placeholder"; // placeholder for username
-        System.out.println("Hello " + name);
-    }
-}
